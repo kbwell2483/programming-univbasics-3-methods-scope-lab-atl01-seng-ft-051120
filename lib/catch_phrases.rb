41e41b00a899648518ac829efeba1ffe9b3697c8
Kelly Blackwell <kellylenorablackwell@gmail.com>
2020-05-06 01:28:14 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,9 +7,9 @@ end
 def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
-  
+end  
+
 def link
   phrase = "It's Dangerous To Go Alone! Take This."
   puts phrase
-  
 end
